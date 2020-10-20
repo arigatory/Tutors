deb864e4067705d3175a5d0142a45cd6ee71f307
Ivan Panchenko <arigatory@gmail.com>
2020-10-20 03:21:10 +0300

    fill db

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from flask import Flask
 from flask import render_template
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 from flask_wtf import FlaskForm
 from jinja2 import environment
@@ -7,8 +9,13 @@ from wtforms import StringField, SubmitField, HiddenField
 
 import data_provider
 
+
 app = Flask(__name__)
 app.secret_key = '42'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///tutors.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
 
 class BookingForm(FlaskForm):
@@ -28,9 +35,57 @@ class Booking:
         self.phone = phone
 
 
+class Booking(db.Model):
+    __tablename__ = 'bookings'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(20))
+    phone = db.Column(db.String(20))
+    weekday = db.Column(db.String(20))
+    hour = db.Column(db.Integer)
+    teacher = db.relationship("Teacher", back_populates='bookings')
+    teacher_id = db.Column(db.Integer, db.ForeignKey('teachers.id'))
+
+
+teachers_goals_association = db.Table('teachers_goals',
+                                      db.Column('teacher_id', db.Integer, db.ForeignKey('teachers.id')),
+                                      db.Column('goal_id', db.Integer, db.ForeignKey('goals.id'))
+                                      )
+
+
+class Teacher(db.Model):
+    __tablename__ = 'teachers'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(20))
+    picture = db.Column(db.String())
+    about = db.Column(db.String())
+    price = db.Column(db.Integer())
+    rating = db.Column(db.Float())
+    bookings = db.relationship("Booking", back_populates='teacher')
+    free_times = db.relationship('FreeTimes', back_populates='teacher')
+    goals = db.relationship('Goal', secondary=teachers_goals_association, back_populates='teachers')
+
+
+class FreeTimes(db.Model):
+    __tablename__ = 'free_times'
+    id = db.Column(db.Integer, primary_key=True)
+    time = db.Column(db.Time)
+    weekday = db.Column(db.String)
+    teacher = db.relationship("Teacher", back_populates='free_times')
+    teacher_id = db.Column(db.Integer, db.ForeignKey('teachers.id'))
+
+
+class Goal(db.Model):
+    __tablename__ = 'goals'
+    id = db.Column(db.Integer, primary_key=True)
+    rus = db.Column(db.String)
+    short = db.Column(db.String)
+    teachers = db.relationship('Teacher', secondary=teachers_goals_association, back_populates='goals')
+
+
 @app.route('/')
 def render_main():
     return render_template('index.html', goals=data_provider.get_goals(), teachers=data_provider.get_teachers(6))
+
 
 @app.route('/all/')
 def render_all():
---
+++ fill_db.py
@@ -0,0 +1,55 @@
+from app import db, Teacher, Booking, FreeTimes, Goal
+import json
+import data_provider
+import datetime
+
+# with open("teachers.json", "w") as f:
+#     json.dump(teachers, f,
+#               ensure_ascii=False,
+#               sort_keys=True,
+#               indent=4,
+#               separators=(',', ': ')
+#               )
+
+
+def load_goals_from_json_to_db():
+    with open("goals.json", "r", encoding='utf8') as f:
+        goals = json.load(f)
+    for k, v in goals.items():
+        goal = Goal(rus=v, short=k)
+        db.session.add(goal)
+    db.session.commit()
+
+
+
+def load_teachers_from_json_to_db():
+    with open("teachers.json", "r", encoding='utf8') as f:
+        teachers = json.load(f)
+        for teacher in teachers:
+            new_teacher = Teacher(name=teacher['name'],
+                                  picture=teacher['picture'],
+                                  price=int(teacher['price']),
+                                  about=str(teacher['about']),
+                                  rating=float(teacher['rating']))
+            db.session.add(new_teacher)
+            goals = teacher['goals']
+            for k in goals:
+                goal = db.session.query(Goal).filter(Goal.short == k).first()
+                if goal:
+                    new_teacher.goals.append(goal)
+            free_times = teacher['free']
+            whole_week = data_provider.whole_week()
+            for weekday_key, weekday_value in whole_week.items():
+                for k, v in free_times[weekday_key].items():
+                    if v:
+                        h = int(k.split(':')[0])
+                        t = datetime.time(h, 00)
+                        db.session.add(FreeTimes(teacher=new_teacher,
+                                                 weekday=weekday_key,
+                                                 time=t))
+        db.session.commit()
+
+
+# load_teachers_from_json_to_db()
+
+# load_goals_from_json_to_db()
