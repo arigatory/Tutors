9ac81290c4f3564053e7403833ed442af7eda7be
Ivan Panchenko <arigatory@gmail.com>
2020-10-22 02:09:27 +0300

    fin

--- app.py
+++ app.py
@@ -1,12 +1,13 @@
 from flask import Flask
 from flask import render_template
+from flask import request
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy.sql.expression import func
 
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, HiddenField
-
+from wtforms import StringField, SubmitField, HiddenField, RadioField
+from wtforms.validators import InputRequired, Length
 
 import data_provider
 
@@ -16,25 +17,19 @@ app.secret_key = '42'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///tutors.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
+
 class BookingForm(FlaskForm):
-    clientName = StringField("Вас зовут")
-    clientPhone = StringField("Ваш телефон")
+    clientName = StringField("Вас зовут", [InputRequired(message="Данное поле являетвся обязательным"),
+                                           Length(min=2, message='Слишком короткое имя')])
+    clientPhone = StringField("Ваш телефон", [InputRequired(message="Данное поле являетвся обязательным"),
+                                              Length(min=5, message='Слишком короткий номер телефона')])
     submit = SubmitField("Записаться на пробный урок")
     clientWeekday = HiddenField("mon")
     clientTime = HiddenField("12:00")
     clientTeacher = HiddenField("10")
-
-
-class Booking:
-    def __init__(self, weekday, hour, name, phone):
-        self.weekday = weekday
-        self.hour = hour
-        self.name = name
-        self.phone = phone
 
 
 class Booking(db.Model):
@@ -46,6 +41,15 @@ class Booking(db.Model):
     hour = db.Column(db.Integer)
     teacher = db.relationship("Teacher", back_populates='bookings')
     teacher_id = db.Column(db.Integer, db.ForeignKey('teachers.id'))
+
+
+class Request(db.Model):
+    __tablename__ = 'requests'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(20))
+    phone = db.Column(db.String(20))
+    goal = db.Column(db.String(20))
+    free_time = db.Column(db.String(20))
 
 
 teachers_goals_association = db.Table('teachers_goals',
@@ -84,16 +88,35 @@ class Goal(db.Model):
     teachers = db.relationship('Teacher', secondary=teachers_goals_association, back_populates='goals')
 
 
+class RequestForm(FlaskForm):
+    clientName = StringField("Вас зовут", [InputRequired(message="Данное поле являетвся обязательным"),
+                                           Length(min=2, message='Слишком короткое имя')])
+    clientPhone = StringField("Ваш телефон", [InputRequired(message="Данное поле являетвся обязательным"),
+                                              Length(min=5, message='Слишком короткий номер телефона')])
+    submit = SubmitField("Найти мне преподавателя")
+    goals = Goal.query.all()
+    choices = []
+    for goal in goals:
+        choices.append((goal.short, goal.rus))
+    goal = RadioField("Какая цель занятий?", choices=choices, default='travel')
+    free_time = RadioField("Сколько времени есть?", choices=[('1-2', '1-2 часа в неделю'),
+                                                             ('3-5', '3-5 часов в неделю'),
+                                                             ('5-7', '5-7 часов в неделю'),
+                                                             ('7-10', '7-10 часов в неделю')], default='3-5')
+
+
 @app.route('/')
 def render_main():
     teachers = Teacher.query.order_by(func.random()).limit(6).all()
-    return render_template('index.html', goals=Goal.query.all(), teachers=teachers)
+    goals = Goal.query.all()
+    return render_template('index.html', goals=goals, teachers=teachers)
 
 
 @app.route('/all/')
 def render_all():
     teachers = Teacher.query.all()
-    return render_template('index.html', goals=Goal.query.all(), teachers=teachers)
+    goals = Goal.query.all()
+    return render_template('index.html', goals=goals, teachers=teachers)
 
 
 @app.route('/goals/<goal>/')
@@ -111,36 +134,57 @@ def render_profiles(teacher_id):
     return render_template('profile.html', teacher=teacher, goals=teacher.goals, week=week)
 
 
-@app.route('/request/')
+@app.route('/request/', methods=['GET', 'POST'])
 def render_request():
-    return render_template('request.html')
+    form = RequestForm()
+    if request.method == "POST" and form.validate():
+        name = form.clientName.data
+        phone = form.clientPhone.data
+        goal = form.goal.data
+        choices = dict(form.goal.choices)
+        goal_label = choices[goal]
+        free_time = form.free_time.data
+        choices = dict(form.free_time.choices)
+        free_time_label = choices[free_time]
+        req = Request(name=name, phone=phone, goal=goal, free_time=free_time)
+        db.session.add(req)
+        db.session.commit()
+        return render_template('request_done.html', name=name, phone=phone, goal=goal_label, free_time=free_time_label)
+    else:
+        return render_template('request.html', form=form)
 
 
-@app.route('/request_done/')
-def render_request_done():
-    return render_template('request_done.html')
-
-
-@app.route('/booking/<int:teacher_id>/<day_of_the_week>/<time>/', methods=['GET', 'POST'])
+@app.route('/booking/<int:teacher_id>/<day_of_the_week>/<int:time>/', methods=['GET', 'POST'])
 def render_booking(teacher_id, day_of_the_week, time):
     teacher = Teacher.query.filter(Teacher.id == teacher_id).first()
     week = data_provider.whole_week()
     form = BookingForm()
-    return render_template('booking.html', teacher=teacher, day_of_the_week=day_of_the_week, time=time, week=week,
-                           form=form)
+
+    if request.method == "POST" and form.validate():
+        name = form.clientName.data
+        weekdays = data_provider.whole_week()
+        weekday = weekdays[form.clientWeekday.data]
+        time = form.clientTime.data
+        phone = form.clientPhone.data
+        booking = Booking(weekday=weekday, hour=time, name=name, phone=phone, teacher_id=teacher_id)
+        db.session.add(booking)
+        db.session.commit()
+        # data_provider.save_booking(booking)
+        return render_template('booking_done.html', name=name, weekday=weekday, time=time, phone=phone)
+    else:
+        return render_template('booking.html', teacher=teacher, day_of_the_week=day_of_the_week, time=time, week=week,
+                               form=form)
 
 
 @app.route('/booking_done/', methods=["GET", "POST"])
 def render_booking_done():
     form = BookingForm()
-    name = form.clientName.data
-    weekdays = data_provider.whole_week()
-    weekday = weekdays[form.clientWeekday.data]
-    time = form.clientTime.data
-    phone = form.clientPhone.data
-    booking = Booking(weekday, time, name, phone)
-    data_provider.save_booking(booking)
-    return render_template('booking_done.html', name=name, weekday=weekday, time=time, phone=phone)
+
+    if form.validate_on_submit():
+        return "OK"
+    else:
+        return "errors"
+    #
 
 
 if __name__ == '__main__':
